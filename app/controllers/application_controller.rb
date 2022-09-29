c8ba19c18083011303da1bd2991c396c23e916a4
Amanda Sakuma <amandasakuma@gmail.com>
2022-09-29 15:09:29 -0400

    commit tree a43322913667862084f91f5169e8ce376e0879c5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_response
+  # before_action :authorize
+
+
+private 
+
+  def invalid_response(exception)
+    render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+  end 
+
+
 
 end
